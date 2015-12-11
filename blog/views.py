a5f45447c9d541144d182961a3af71fada0bb94a
firdamaryana <firda_maryana94@yahoo.com>
2015-12-11 15:20:28 +0700

    first commit

---
+++ blog/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+from django.shortcuts import render, get_object_or_404
+
+def about(request):
+
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': "I am bold font from the context"}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'blog/about.html', context_dict)
+
+def home(request):
+
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': "I am bold font from the context"}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'blog/home.html', context_dict)
---
+++ mysite/settings-pythonanywhere.py~
@@ -0,0 +1,11 @@
+from mysite.settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'firdamaryana$mysite',
+    	'USER': 'firdamaryana',
+	    'PASSWORD' : 'mysqlpassword',
+	    'HOST':'mysql.server'
+    }
+}
